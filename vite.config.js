51f9a599f638257d0af75da0b616eea26918f93a
Mubashir271 <jmubashir278@gmail.com>
2026-05-25 11:19:24 +0500

    Live Link url updated

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Qissa-e-Urdu/',
+  base: process.env.VITE_BASE_URL || '/Qissa-e-Urdu/',
 })
